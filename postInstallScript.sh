3be79abf62db1ead56a1674bb24113c9bd56c4c1
systeminstall2 <156584743+systeminstall2@users.noreply.github.com>
2024-02-13 13:09:34 +0100

    Update postInstallScript.sh

--- postInstallScript.sh
+++ postInstallScript.sh
@@ -41,7 +41,7 @@ sudo echo " " && # prevent having to enter password again
 
 yay -Syu --noconfirm etcher-bin ventoy-bin sxhkd protonvpn-gui rustdesk-bin syncthing-gtk visual-studio-code-bin burpsuite oh-my-zsh-plugin-autosuggestions oh-my-zsh-plugin-syntax-highlighting && sudo pacman -S --noconfirm ttf-hack polkit-kde-agent brasero rofi feh polybar tree chromium network-manager-applet cmake firefox-developer-edition wmname gnome-disk-utility gnu-netcat gparted kate keepassxc libreoffice-still btop prusa-slicer maim neofetch neovim nmap p7zip python-pip ripgrep sxiv syncthing terminator torbrowser-launcher ufw unzip vim virtualbox wget whois xclip zenity zsh && sudo chsh -s /usr/bin/zsh && sudo chsh -s /usr/bin/zsh n0ne && sudo pacman -Rns --noconfirm firewalld
 
-cp ~/systeminstall/wallpaper.jpg ~/Documents/ && feh --bg-scale ~/Documents/wallpaper.jpg
+cp ~/systeminstall/thisImage ~/Documents/ && feh --bg-scale ~/Documents/thisImage
 
 # Setting up zsh plugins #####################
 
