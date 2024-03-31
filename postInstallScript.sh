d5af94f5e81b2fb3774d8e1ec0c1805ef1ba1f74
systeminstall2 <156584743+systeminstall2@users.noreply.github.com>
2024-04-01 00:42:06 +0200

    Update postInstallScript.sh

--- postInstallScript.sh
+++ postInstallScript.sh
@@ -2,6 +2,34 @@
 
 git clone https://github.com/systeminstall/systeminstall ~/systeminstall 
 
+#Installing all packages ##########################################################
+
+sudo echo " " && # prevent having to enter password again 
+
+yay -Syu --noconfirm etcher-bin ventoy-bin sxhkd protonvpn-gui rustdesk-bin syncthing-gtk visual-studio-code-bin burpsuite oh-my-zsh-plugin-autosuggestions oh-my-zsh-plugin-syntax-highlighting && sudo pacman -S --noconfirm ttf-hack polkit-kde-agent brasero rofi feh polybar tree chromium network-manager-applet cmake firefox-developer-edition wmname gnome-disk-utility gnu-netcat gparted kate keepassxc libreoffice-still btop prusa-slicer maim neofetch neovim nmap p7zip python-pip ripgrep sxiv syncthing terminator torbrowser-launcher ufw unzip vim virtualbox wget whois xclip zenity zsh && sudo chsh -s /usr/bin/zsh && sudo chsh -s /usr/bin/zsh n0ne && sudo pacman -Rns --noconfirm firewalld
+
+cp ~/systeminstall/thisImage ~/Documents/ && feh --bg-scale ~/Documents/thisImage
+
+# Setting up zsh plugins #####################
+
+sudo mkdir -p /usr/share/zsh/plugins && sudo mkdir -p /usr/share/zsh/plugins/zsh-syntax-highlighting && sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /usr/share/zsh/plugins/zsh-syntax-highlighting  && sudo git clone https://github.com/zsh-users/zsh-autosuggestions.git /usr/share/zsh-autosuggestions
+
+# Disable endeavour pop Up shit ########
+
+eos-welcome --disable
+
+# Set up start up programs
+
+sudo systemctl enable bluetooth
+
+# Install Nerdfonts for emojis
+
+mkdir fonts && cd fonts && wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/Hack.zip" && unzip Hack.zip && sudo mkdir -p /usr/local/share/fonts && rm Hack.zip && sudo mv * /usr/local/share/fonts/ && cd ..
+
+#Fix weird themeing stuff if kde and bspwm are installed alongside
+
+cat ~/systeminstall/.xprofile > ~/.xprofile
+
 # Writing zshrc ##################################
 
 cat ~/systeminstall/zshrc > ~/.zshrc
@@ -34,33 +62,5 @@ mkdir -p ~/.config/terminator && cat ~/systeminstall/terminator > ~/.config/term
 # Writing nvim #########################################
 
 rm -rf ~/.config/nvim && cp ~/systeminstall/nvim ~/.config/ -r
-
-#Installing all packages ##########################################################
-
-sudo echo " " && # prevent having to enter password again 
-
-yay -Syu --noconfirm etcher-bin ventoy-bin sxhkd protonvpn-gui rustdesk-bin syncthing-gtk visual-studio-code-bin burpsuite oh-my-zsh-plugin-autosuggestions oh-my-zsh-plugin-syntax-highlighting && sudo pacman -S --noconfirm ttf-hack polkit-kde-agent brasero rofi feh polybar tree chromium network-manager-applet cmake firefox-developer-edition wmname gnome-disk-utility gnu-netcat gparted kate keepassxc libreoffice-still btop prusa-slicer maim neofetch neovim nmap p7zip python-pip ripgrep sxiv syncthing terminator torbrowser-launcher ufw unzip vim virtualbox wget whois xclip zenity zsh && sudo chsh -s /usr/bin/zsh && sudo chsh -s /usr/bin/zsh n0ne && sudo pacman -Rns --noconfirm firewalld
-
-cp ~/systeminstall/thisImage ~/Documents/ && feh --bg-scale ~/Documents/thisImage
-
-# Setting up zsh plugins #####################
-
-sudo mkdir -p /usr/share/zsh/plugins && sudo mkdir -p /usr/share/zsh/plugins/zsh-syntax-highlighting && sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /usr/share/zsh/plugins/zsh-syntax-highlighting  && sudo git clone https://github.com/zsh-users/zsh-autosuggestions.git /usr/share/zsh-autosuggestions
-
-# Disable endeavour pop Up shit ########
-
-eos-welcome --disable
-
-# Set up start up programs
-
-sudo systemctl enable bluetooth
-
-# Install Nerdfonts for emojis
-
-mkdir fonts && cd fonts && wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/Hack.zip" && unzip Hack.zip && sudo mkdir -p /usr/local/share/fonts && rm Hack.zip && sudo mv * /usr/local/share/fonts/ && cd ..
-
-#Fix weird themeing stuff if kde and bspwm are installed alongside
-
-cat ~/systeminstall/.xprofile > ~/.xprofile
 
 echo "\n\nInstallation finished\n\n\nDon't forget to install the correct driver for this device!"
