af10b2fc1bc5511b220226def12c0b7abae5eecd
systeminstall2 <156584743+systeminstall2@users.noreply.github.com>
2024-05-02 20:40:08 +0200

    Update postInstallScript.sh

--- postInstallScript.sh
+++ postInstallScript.sh
@@ -2,6 +2,8 @@ printf "Remove the password and reboot or stay at the pc/vm at all times, becaus
 
 for i in {9..0};do echo "Starting Script in $i seconds...READ ABOVE\!" && sleep 1;done
 
+sudo chsh -s /usr/bin/zsh
+sudo chsh -s /usr/bin/zsh n0ne
 
 # Getting all the files
 
@@ -22,8 +24,6 @@ source /etc/profile
 
 yay -Syu --noconfirm --sudoloop etcher-bin cargo ventoy-bin sxhkd protonvpn-gui rustdesk-bin syncthing-gtk visual-studio-code-bin burpsuite oh-my-zsh-plugin-autosuggestions oh-my-zsh-plugin-syntax-highlighting
 sudo pacman -S --noconfirm ripgrep ttf-hack python-pynvim polkit-kde-agent brasero rofi feh polybar tree chromium network-manager-applet cmake firefox-developer-edition wmname gnome-disk-utility gnu-netcat gparted kate keepassxc libreoffice-still btop prusa-slicer maim fastfetch neovim nmap p7zip python-pip ripgrep sxiv syncthing terminator torbrowser-launcher ufw unzip vim virtualbox wget whois xclip zenity zsh
-sudo chsh -s /usr/bin/zsh
-sudo chsh -s /usr/bin/zsh n0ne
 sudo pacman -Rns --noconfirm firewalld
 
 curl -o- "https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh" | bash
